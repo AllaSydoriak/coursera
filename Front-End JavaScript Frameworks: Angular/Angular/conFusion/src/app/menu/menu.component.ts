b117649102d16178ce55141b68559aac56222afe
AllaSydoriak <alochka12@ukr.net>
2017-10-16 16:02:20 +0300

    Basic Service

--- Front-End JavaScript Frameworks: Angular/Angular/conFusion/src/app/menu/menu.component.ts
+++ Front-End JavaScript Frameworks: Angular/Angular/conFusion/src/app/menu/menu.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
+import { DishService } from '../services/dish.service';
 
 @Component({
   selector: 'app-menu',
@@ -12,17 +12,18 @@ import { DISHES } from '../shared/dishes';
 
 export class MenuComponent implements OnInit {
 
-  dishes = DISHES;
+  dishes: Dish[];
 
   selectedDish: Dish;
 
-  constructor() { }
+  constructor(private dishService: DishService) { }
 
   onSelect(dish: Dish) {
     this.selectedDish = dish;
   }
 
   ngOnInit() {
+    this.dishes = this.dishService.getDishes();
   }
 
 
