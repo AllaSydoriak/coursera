17f9c0c1b9f3415b817089e6e8108b1228b69ddd
AllaSydoriak <alochka12@ukr.net>
2017-11-05 16:29:30 +0200

    Template Forms Part 1

--- Front-End JavaScript Frameworks: Angular/Angular/conFusion/src/app/app.module.ts
+++ Front-End JavaScript Frameworks: Angular/Angular/conFusion/src/app/app.module.ts
@@ -22,6 +22,7 @@ import { LeaderService } from './services/leader.service';
 import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -42,6 +44,9 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FormsModule,
     HttpModule,
     AppRoutingModule
+  ],
+  entryComponents: [
+    LoginComponent
   ],
   providers: [
     DishService,
