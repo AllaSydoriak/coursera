a1d201e4eee557089186254c1dd1062c7be0a682
AllaSydoriak <alochka12@ukr.net>
2017-11-10 23:21:38 +0200

    Animations Part 1

--- Front-End JavaScript Frameworks: Angular/Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ Front-End JavaScript Frameworks: Angular/Angular/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -11,10 +11,25 @@ import { Location } from '@angular/common';
 
 import 'rxjs/add/operator/switchMap';
 
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+        state('shown', style({
+            transform: 'scale(1.0)',
+            opacity: 1
+        })),
+        state('hidden', style({
+            transform: 'scale(0.5)',
+            opacity: 0
+        })),
+        transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
 
@@ -30,6 +45,8 @@ export class DishdetailComponent implements OnInit {
   comment: Comment;
 
   errMess: string;
+
+  visibility = 'shown';
 
   formErrors = {
     'author': '',
@@ -58,9 +75,9 @@ export class DishdetailComponent implements OnInit {
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds,
       errmess => this.errMess = <any>errmess);
-    this.route.params
-      .switchMap((params: Params) => { return this.dishservice.getDish(+params['id']); })
-      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
+      this.route.params
+        .switchMap((params: Params) => { this.visibility = 'hidden'; return this.dishservice.getDish(+params['id']); })
+        .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; },
             errmess => { this.dish = null; this.errMess = <any>errmess; });
   }
 
