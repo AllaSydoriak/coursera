527511bf955c139aff4c26942414f92e37b36658
AllaSydoriak <alochka12@ukr.net>
2017-12-09 14:22:56 +0200

    NodeJS

--- Front-End JavaScript Frameworks: Angular/Angular/conFusion/src/app/header/header.component.ts
+++ Front-End JavaScript Frameworks: Angular/Angular/conFusion/src/app/header/header.component.ts
@@ -1,21 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { MdDialog, MdDialogRef } from '@angular/material';
-import { LoginComponent } from '../login/login.component';
+import { MdDialog, MdDialogRef } from "@angular/material";
+import { LoginComponent } from "../login/login.component";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent implements OnInit {
+  constructor(public dialog: MdDialog) {}
 
-  constructor(public dialog: MdDialog ) { }
-
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   openLoginForm() {
-    this.dialog.open(LoginComponent, {width: '500px', height: '450px'});
+    this.dialog.open(LoginComponent, { width: "500px", height: "450px" });
   }
 }
