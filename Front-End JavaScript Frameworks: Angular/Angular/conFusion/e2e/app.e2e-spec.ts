087de7e00154b5b43786cd49089b14964dad65c9
AllaSydoriak <alochka12@ukr.net>
2017-12-11 18:32:11 +0200

    End-to-End Test

--- Front-End JavaScript Frameworks: Angular/Angular/conFusion/e2e/app.e2e-spec.ts
+++ Front-End JavaScript Frameworks: Angular/Angular/conFusion/e2e/app.e2e-spec.ts
@@ -1,5 +1,7 @@
 import { AppPage } from './app.po';
 
+import { browser } from 'protractor';
+
 describe('con-fusion App', () => {
   let page: AppPage;
 
@@ -7,8 +9,18 @@ describe('con-fusion App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should enter a new comment for the first dish', () => {
+    page.navigateTo('/dishdetail/0');
+
+    let newAuthor = page.getElement('input[type=text]');
+    newAuthor.sendKeys('Test Author');
+
+    let newComment = page.getElement('textarea');
+    newComment.sendKeys('Test Comment');
+
+    let newSubmitButton = page.getElement("button[type=submit]");
+    newSubmitButton.click();
+
+    browser.pause();
   });
 });
