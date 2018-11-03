de7b1336042905f57928949f13bcde3ba185aeed
AllaSydoriak <alochka12@ukr.net>
2018-11-03 20:34:01 +0200

    Assignment 1

---
+++ Front-End Web Development with React/confusion/src/components/DishdetailComponent.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+
+export default class Dishdetail extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderDish = dish => {
+    return (
+      <Card>
+        <CardImg top src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    );
+  };
+
+  renderComments = comments => {
+    return (
+      <div>
+        <h4>Comments</h4>
+        {comments !== null ? (
+          <div>
+            <ul class="list-unstyled">
+              {comments.map(item => (
+                <li>
+                  <p>{item.comment}</p>
+                  <p>{`-- ${item.author}, ${item.date}`}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <div />
+        )}
+      </div>
+    );
+  };
+
+  render() {
+    const { dish } = this.props;
+    if (dish != null)
+      return (
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">{this.renderDish(dish)}</div>
+          <div className="col-12 col-md-5 m-1">
+            {this.renderComments(dish.comments)}
+          </div>
+        </div>
+      );
+    else return <div />;
+  }
+}
