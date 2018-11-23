e5b81d161d7dde84e24d948bfec3c4392236f521
Alla Sydoriak <alla@asd-team.com>
2018-11-23 11:17:02 +0200

    Header and Footer

--- Front-End Web Development with React/confusion/src/components/MainComponent.js
+++ Front-End Web Development with React/confusion/src/components/MainComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./MenuComponent";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
 import Dishdetail from "./DishdetailComponent";
 import { DISHES } from "../shared/dishes";
 
@@ -25,17 +27,19 @@ class Main extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Header />
         <Menu
           dishes={this.state.dishes}
           onClick={dishId => this.onDishSelect(dishId)}
         />
-        <Dishdetail
+        <DishDetail
           dish={
             this.state.dishes.filter(
               dish => dish.id === this.state.selectedDish
             )[0]
           }
         />
+        <Footer />
       </div>
     );
   }
